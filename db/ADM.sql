2cd9113dbd066bb5089e1a84954f4745fcaf2c3b
rohmanahmad <rohmanmail@gmail.com>
2014-09-29 10:32:05 +0700

    d

--- db/ADM.sql
+++ db/ADM.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.1.12
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Sep 27, 2014 at 06:03 PM
--- Server version: 5.5.38-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.4
+-- Host: 127.0.0.1
+-- Generation Time: Sep 29, 2014 at 05:30 AM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `ADM`
+-- Database: `adm`
 --
 
 -- --------------------------------------------------------
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `fb_applications` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `app_name` varchar(20) NOT NULL,
   `appId` varchar(100) NOT NULL,
   `appSecret` varchar(100) NOT NULL,
   `return_url` tinytext NOT NULL,
@@ -36,14 +37,16 @@ CREATE TABLE IF NOT EXISTS `fb_applications` (
   `token` tinytext NOT NULL,
   `status` tinyint(1) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `fb_applications`
 --
 
-INSERT INTO `fb_applications` (`ID`, `appId`, `appSecret`, `return_url`, `homeurl`, `fbPermissions`, `token`, `status`) VALUES
-(1, '142895449211373', '3dbcdb2924d107bd28293f99ba0634e4', 'http://rezstore.com/facebook/process.php', 'http://rezstore.com/facebook/', 'publish_stream,manage_pages', 'CAACB9nB9ge0BAFzOdce8Y9yqwQbfc2uvvCgFNRvvrqs6x6FN98PsRG1MmiKsxw6DZAxOwqEtBQfQAZAtrXMaLsThTZBl279QC20dGskPGbNJhx7UtiOxEEr8AxzHK6ciAlZAYxCDBJcngM2wmMNz1Jn1cIZCAGix9YWMckTTZAU6ndSlsvz7kwckFJ77N5EB1sRHJBAcCPg24CCajLnGrQ', 1);
+INSERT INTO `fb_applications` (`ID`, `app_name`, `appId`, `appSecret`, `return_url`, `homeurl`, `fbPermissions`, `token`, `status`) VALUES
+(1, 'rezstore', '142895449211373', '3dbcdb2924d107bd28293f99ba0634e4', 'http://rezstore.com/facebook/process.php', 'http://rezstore.com/facebook/', 'publish_stream,manage_pages', 'CAACB9nB9ge0BAFzOdce8Y9yqwQbfc2uvvCgFNRvvrqs6x6FN98PsRG1MmiKsxw6DZAxOwqEtBQfQAZAtrXMaLsThTZBl279QC20dGskPGbNJhx7UtiOxEEr8AxzHK6ciAlZAYxCDBJcngM2wmMNz1Jn1cIZCAGix9YWMckTTZAU6ndSlsvz7kwckFJ77N5EB1sRHJBAcCPg24CCajLnGrQ', 0),
+(2, 'rohman', '142895449211333', '3dbcdb2924d107bd28293f99ba0634e4', 'http://rezstore.com/facebook/process.php', 'http://rezstore.com/facebook/', 'publish_stream,manage_pages', 'CAACB9nB9ge0BAFzOdce8Y9yqwQbfc2uvvCgFNRvvrqs6x6FN98PsRG1MmiKsxw6DZAxOwqEtBQfQAZAtrXMaLsThTZBl279QC20dGskPGbNJhx7UtiOxEEr8AxzHK6ciAlZAYxCDBJcngM2wmMNz1Jn1cIZCAGix9YWMckTTZAU6ndSlsvz7kwckFJ77N5EB1sRHJBAcCPg24CCajLnGrQ', 0),
+(3, 'werwe', 'werwer', 'werwer', 'wer', 'werwe', 'rewrwer', 'werwerer', 1);
 
 -- --------------------------------------------------------
 
@@ -84,15 +87,23 @@ CREATE TABLE IF NOT EXISTS `fb_messages_post` (
   `image` tinytext NOT NULL,
   `status` tinyint(4) NOT NULL,
   PRIMARY KEY (`ID_post`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
+
+--
+-- Dumping data for table `fb_messages_post`
+--
+
+INSERT INTO `fb_messages_post` (`ID_post`, `date_post`, `page_id`, `messages`, `url`, `image`, `status`) VALUES
+(27, '2014-09-10', '338081799692389', 'masuk', 'github.com', '1054727.jpg', 0),
+(34, '2014-09-16', '338081799692389', 'jangan masuk kamar saya', 'github.com', '2230928.jpg', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `fb_UID`
+-- Table structure for table `fb_uid`
 --
 
-CREATE TABLE IF NOT EXISTS `fb_UID` (
+CREATE TABLE IF NOT EXISTS `fb_uid` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `UID` varchar(30) NOT NULL,
   `url` tinytext NOT NULL,
@@ -101,10 +112,10 @@ CREATE TABLE IF NOT EXISTS `fb_UID` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
--- Dumping data for table `fb_UID`
+-- Dumping data for table `fb_uid`
 --
 
-INSERT INTO `fb_UID` (`ID`, `UID`, `url`, `status`) VALUES
+INSERT INTO `fb_uid` (`ID`, `UID`, `url`, `status`) VALUES
 (1, '100005343461657', 'https://www.facebook.com/cobasaja123', 1);
 
 -- --------------------------------------------------------
@@ -154,15 +165,15 @@ CREATE TABLE IF NOT EXISTS `twitter_messages_post` (
 --
 
 INSERT INTO `twitter_messages_post` (`ID_post`, `date_post`, `page_id`, `messages`, `url`, `image`, `status`) VALUES
-(6, '2014-09-10', '2832778561', 'janganmasuk', 'github.com/rezstore', '5760127.jpg', 0);
+(6, '2014-09-10', '2832778561', 'janganmasuk masuk', 'github.com/rezstore', '5760127.jpg', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `twitter_UID`
+-- Table structure for table `twitter_uid`
 --
 
-CREATE TABLE IF NOT EXISTS `twitter_UID` (
+CREATE TABLE IF NOT EXISTS `twitter_uid` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `UID` varchar(30) NOT NULL,
   `user_name` varchar(30) NOT NULL,
@@ -172,10 +183,10 @@ CREATE TABLE IF NOT EXISTS `twitter_UID` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `twitter_UID`
+-- Dumping data for table `twitter_uid`
 --
 
-INSERT INTO `twitter_UID` (`ID`, `UID`, `user_name`, `url`, `status`) VALUES
+INSERT INTO `twitter_uid` (`ID`, `UID`, `user_name`, `url`, `status`) VALUES
 (1, '2832778561', 'rezstore', 'https://twitter.com/rezstoreindo', 1),
 (2, '1223463476', 'rohmanahmad', 'https://twitter.com/R0hmanAhmad', 1);
 
@@ -191,14 +202,15 @@ CREATE TABLE IF NOT EXISTS `user_accounts` (
   `password` tinytext NOT NULL,
   `status` tinyint(4) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `user_accounts`
 --
 
 INSERT INTO `user_accounts` (`ID`, `username`, `password`, `status`) VALUES
-(1, 'rohman', 'BmlVagAmV3QFOQ==', 1);
+(1, 'rohman', 'BmlVagAmV3QFOQ==', 1),
+(2, 'laptop', '67bfP9hWnlUePxIQJFkheIF8pZjWYpnBtSxWC5Sld0t9h0gEZjDVw55kYeUtr9RG3+rhoKdS6+gzRUdRKUOh2A==', 1);
 
 -- --------------------------------------------------------
 
@@ -213,7 +225,7 @@ CREATE TABLE IF NOT EXISTS `user_activities` (
   `type` varchar(20) NOT NULL,
   `message` tinytext NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `user_activities`
@@ -221,7 +233,14 @@ CREATE TABLE IF NOT EXISTS `user_activities` (
 
 INSERT INTO `user_activities` (`ID`, `user`, `date_time`, `type`, `message`) VALUES
 (3, 'rohman', '2014-09-27 17:00:48', 'facebook', 'menambah record baru dengan tgl post=2014-09-17'),
-(4, 'rohman', '2014-09-27 17:01:57', 'twitter', 'menambah record baru dengan tgl post=2014-09-10');
+(4, 'rohman', '2014-09-27 17:01:57', 'twitter', 'menambah record baru dengan tgl post=2014-09-10'),
+(5, 'laptop', '2014-09-27 22:36:58', 'twitter', 'menambah record baru dengan tgl post=2014-09-09'),
+(6, 'laptop', '2014-09-27 22:45:39', 'twitter', 'mengubah record dengan ID=6'),
+(7, 'laptop', '2014-09-27 22:47:10', 'facebook', 'menambah record baru dengan tgl post=2014-09-10'),
+(8, 'laptop', '2014-09-28 08:05:07', 'facebook', 'menambah record baru dengan tgl post=2014-09-09'),
+(9, 'laptop', '2014-09-28 08:05:53', 'facebook', 'hapus record dengan id 33'),
+(10, 'laptop', '2014-09-28 08:09:22', 'facebook', 'menambah record baru dengan tgl post=2014-09-16'),
+(11, 'laptop', '2014-09-28 17:05:49', 'facebook', 'input aplikasi facebook baru dengan appId=werwer');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
